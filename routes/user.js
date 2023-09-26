d37daa3aca02895e8cda84427303ded19d384d5e
Hasan Kaya <hasankayaaa53@gmail.com>
2023-09-26 11:01:02 +0300

    anasayfa olusturma

--- routes/user.js
+++ routes/user.js
@@ -4,7 +4,45 @@ const router = express.Router();
 
 
 router.get('/',(req,res,next)=>{
-    res.render('index',{pageTitle:'Home'});
+    let productList = [
+        {
+            href: "product-6.html",
+            front_image: "product-6.jpg",
+            back_image: "",
+            category:"Dress",
+            name: "Cotton floral print Dress",
+            price: "40",
+            discount: "50",
+            discount_price: "20",
+            currency: "TL",
+            InsertDate: "2020-08-01"
+        },
+        {
+            href: "product-6.html",
+            front_image: "product-6.jpg",
+            back_image: "product-121.jpg",
+            category:"Dress",
+            name: "Cotton floral print Dress",
+            price: "30",
+            discount: "0",
+            discount_price: "0",
+            currency: "TL",
+            InsertDate: "2023-09-01"
+        },
+        {
+            href: "product-6.html",
+            front_image: "product-6.jpg",
+            back_image: "product-121.jpg",
+            category:"Dress",
+            name: "Cotton floral print Dress",
+            price: "50",
+            discount: "0",
+            discount_price: "0",
+            currency: "TL",
+            InsertDate: "2023-08-01"
+        }
+    ];
+    res.render('index',{pageTitle:'Home',productList:productList});
 });
 
 
